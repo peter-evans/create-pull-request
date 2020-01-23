df0d07269bcd9ce82c03ef0cef8297d6b709fdfa
Peter Evans <peter-evans@users.noreply.github.com>
2020-01-23 16:59:17 +0900

    Vendor python dependencies

--- dist/index.js
+++ dist/index.js
@@ -1018,7 +1018,9 @@ async function run() {
     await exec.exec("pip", [
       "install",
       "--requirement",
-      `${src}/requirements.txt`
+      `${src}/requirements.txt`,
+      "--no-index",
+      `--find-links=${__dirname}/vendor`
     ]);
 
     // Fetch action inputs
