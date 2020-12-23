9b632d8eba4ad9acf9621d059f7b0ece6b4125a3
peter-evans <peter-evans@users.noreply.github.com>
2020-12-23 04:46:13 +0000

    [CI] test built

--- dist/index.js
+++ dist/index.js
@@ -134,8 +134,9 @@ function createOrUpdateBranch(git, commitMessage, base, branch, branchRemoteName
         // Perform fetch and reset the working base
         // Commits made during the workflow will be removed
         if (workingBaseType == WorkingBaseType.Branch) {
-            core.info(`Resetting working base branch '${workingBase}' to its remote`);
-            yield git.fetch([`${workingBase}:${workingBase}`], baseRemote, ['--force']);
+            core.info(`Resetting working base branch '${workingBase}'`);
+            yield git.checkout(workingBase);
+            yield git.exec(['reset', '--hard', `${baseRemote}/${workingBase}`]);
         }
         // If the working base is not the base, rebase the temp branch commits
         // This will also be true if the working base type is a commit
@@ -162,7 +163,7 @@ function createOrUpdateBranch(git, commitMessage, base, branch, branchRemoteName
             // The pull request branch does not exist
             core.info(`Pull request branch '${branch}' does not exist yet.`);
             // Create the pull request branch
-            yield git.checkout(branch, 'HEAD');
+            yield git.checkout(branch, tempBranch);
             // Check if the pull request branch is ahead of the base
             result.hasDiffWithBase = yield isAhead(git, base, branch);
             if (result.hasDiffWithBase) {
