ec02db74e995211d5d9046b4dac9e40722601c08
Peter Evans <18365890+peter-evans@users.noreply.github.com>
2024-08-07 14:38:25 +0000

    fix format

--- __test__/git-command-manager.int.test.ts
+++ __test__/git-command-manager.int.test.ts
@@ -14,9 +14,13 @@ describe('git-command-manager integration tests', () => {
     const parent = await git.getCommit('HEAD^')
     const commit = await git.getCommit('HEAD')
     expect(parent.subject).toEqual('initial commit')
-    expect(parent.changes).toEqual([{"mode": "100644", "status": "A", "path": "README.md"}])
+    expect(parent.changes).toEqual([
+      {mode: '100644', status: 'A', path: 'README.md'}
+    ])
     expect(commit.subject).toEqual('add sparkles')
     expect(commit.parents[0]).toEqual(parent.sha)
-    expect(commit.changes).toEqual([{"mode": "100644", "status": "M", "path": "README.md"}])
+    expect(commit.changes).toEqual([
+      {mode: '100644', status: 'M', path: 'README.md'}
+    ])
   })
 })
