5294a5ed9d55033b3242cb808befa256e3a13d21
Peter Evans <18365890+peter-evans@users.noreply.github.com>
2024-08-16 15:44:34 +0100

    add maintainer-can-modify input

--- src/main.ts
+++ src/main.ts
@@ -28,7 +28,8 @@ async function run(): Promise<void> {
       reviewers: utils.getInputAsArray('reviewers'),
       teamReviewers: utils.getInputAsArray('team-reviewers'),
       milestone: Number(core.getInput('milestone')),
-      draft: core.getBooleanInput('draft')
+      draft: core.getBooleanInput('draft'),
+      maintainerCanModify: core.getBooleanInput('maintainer-can-modify')
     }
     core.debug(`Inputs: ${inspect(inputs)}`)
 
