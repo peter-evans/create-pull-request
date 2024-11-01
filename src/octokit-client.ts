998218f18d7a9f2920baa85a0d5537621176b208
Ben McCann <322311+benmccann@users.noreply.github.com>
2024-11-01 11:35:35 -0700

    chore: don't bundle undici

--- src/octokit-client.ts
+++ src/octokit-client.ts
@@ -4,7 +4,7 @@ import {paginateRest} from '@octokit/plugin-paginate-rest'
 import {restEndpointMethods} from '@octokit/plugin-rest-endpoint-methods'
 import {throttling} from '@octokit/plugin-throttling'
 import {getProxyForUrl} from 'proxy-from-env'
-import {ProxyAgent, fetch as undiciFetch} from 'undici'
+import {createFetch} from 'node-fetch-native/proxy'
 export {RestEndpointMethodTypes} from '@octokit/plugin-rest-endpoint-methods'
 // eslint-disable-next-line import/no-unresolved
 export {OctokitOptions} from '@octokit/core/dist-types/types'
@@ -33,23 +33,12 @@ export const throttleOptions = {
   }
 }
 
-const proxyFetch =
-  (proxyUrl: string): typeof undiciFetch =>
-  (url, opts) => {
-    return undiciFetch(url, {
-      ...opts,
-      dispatcher: new ProxyAgent({
-        uri: proxyUrl
-      })
-    })
-  }
-
 // Octokit plugin to support the standard environment variables http_proxy, https_proxy and no_proxy
 function autoProxyAgent(octokit: OctokitCore) {
   octokit.hook.before('request', options => {
     const proxy = getProxyForUrl(options.baseUrl)
     if (proxy) {
-      options.request.fetch = proxyFetch(proxy)
+      options.request.fetch = createFetch(proxy)
     }
   })
 }
