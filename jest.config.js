8b4b9968cbd4e3968f81e3703d3932f61ad39815
Peter Evans <18365890+peter-evans@users.noreply.github.com>
2026-06-24 14:43:06 +0100

    build(deps): update to jest 30 (#4388)

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
   moduleFileExtensions: ['js', 'ts'],
   testEnvironment: 'node',
   testMatch: ['**/*.test.ts'],
-  testRunner: 'jest-circus/runner',
+
   transform: {
     '^.+\\.ts$': 'ts-jest'
   },
